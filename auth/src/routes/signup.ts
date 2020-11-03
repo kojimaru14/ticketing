1c70d9906266c485097770e737ef133eeae7c677
jiko <>
2020-11-03 10:18:05 +0900

    Adding express-validation to signup.ts

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,28 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/users/signup', (req, res)=> {
-  res.send('Hi there');
-});
+router.post('/api/users/signup', [
+    body('email') // adding "body" module as middleware
+      .isEmail()
+      .withMessage('Email must be valid'),
+    body('password')
+      .trim()     // sanitisation (removing leading space, etc.)
+      .isLength({ min: 4, max: 20})
+      .withMessage('Password must be between 4 and 20 characters')
+  ], (req: Request, res: Response)=> {
+    const errors = validationResult(req);
+
+    if( !errors.isEmpty() ) {
+      return res.status(400).send(errors.array());
+    }
+
+    const { email, password } = req.body;
+    console.log('Creating a user..');
+
+    res.send('Hi there');
+  }
+);
 
 export { router as signupRouter };
