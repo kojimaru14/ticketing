c0830b0165b460bfff925f68afd811a28a883600
jiko <>
2020-11-07 08:38:57 +0900

    [Adding session support] Generating a JWT

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -3,6 +3,7 @@ import { body, validationResult } from 'express-validator';
 import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
@@ -29,6 +30,15 @@ router.post('/api/users/signup', [
 
     const user = User.build({email, password});
     await user.save();  // building user doesn't save the record to DB, we need to save it.
+
+    // Generate JWT
+    const userJwt = jwt.sign({
+      id: user.id,
+      email: user.email
+    }, 'privatekeystring');
+
+    // Store it on session object
+    req.session = { jwt: userJwt };
 
     res.status(201).send(user);
   }
