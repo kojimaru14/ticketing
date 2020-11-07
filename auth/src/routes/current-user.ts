1860e9d3df363433fdb185a7058612f28b98e027
jiko <>
2020-11-07 14:27:19 +0900

    Returning the current user (Verifying the user from JWT set in cookie)

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,23 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req, res)=> {
-  res.send('Hi there');
+  if(!req.session?.jwt) { // <= this syntax is the same as this: if(!req.session || !req.session.jwt) {
+    return res.send({ currentUser: null});
+  }
+
+  try {
+    const payload = jwt.verify(
+      req.session.jwt, 
+      process.env.JWT_KEY!
+    );  // if JSON web token is tampered with (or seen as invalid), verify method will thrown an exception
+    res.send({currentUser: payload});
+  } catch (err) {
+    res.send({currentUser: null});
+  }
+  
 });
 
 export { router as currentUserRouter };
