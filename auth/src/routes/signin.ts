738e16747f50e099e30166633874a8b589830d17
jiko <>
2020-11-07 13:58:26 +0900

    [Signin flow] Refactoring validation into a separate middleware

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
 
-import { RequestValidationError } from '../errors/request-validation-error';
+import { validateRequest } from '../middlewares/validate-request';
+
 const router = express.Router();
 
 router.post('/api/users/signin', 
@@ -14,12 +15,8 @@ router.post('/api/users/signin',
       .notEmpty()
       .withMessage('You must supply a password')
   ],
+  validateRequest,
   (req: Request, res: Response)=> {
-    const errors = validationResult(req);
-
-    if ( !errors.isEmpty() ) {
-      throw new RequestValidationError(errors.array());
-    }
     res.send('Hi there');
   }
 );
