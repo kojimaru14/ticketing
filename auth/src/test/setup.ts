9ed0c7eff85efaab603d39533dee37b2b4ebe730
jiko <>
2020-11-08 21:39:29 +0900

    [Testing] Refactoring the process for getting cookie into a global "signin" function

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,7 +1,16 @@
+import request from 'supertest';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 
 import { app } from '../app';
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): Promise<string[]> // Augment a function called "signin" that returns a promise (a promise that returns array of string when resolved)
+    }
+  }
+}
 
 let mongo: any;
 
@@ -32,3 +41,19 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+global.signin = async () => {
+  const email = 'test@test.com';
+  const password = 'password';
+
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email,
+      password
+    })
+    .expect(201);
+
+  const cookie = response.get('Set-Cookie');
+  return cookie;
+};
