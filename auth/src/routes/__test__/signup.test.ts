93c97e6328dba0a9c8469dc19a2963805b946828
jiko <>
2020-11-08 20:14:11 +0900

    [Testing] Testing invalid input on signup endpoint

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -10,3 +10,57 @@ it('returns a 201 on a successful signup', async () => {
     })
     .expect(201);
 });
+
+it('returns a 400 with an invalid email', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: "test@test",
+      password: "password"
+    })
+    .expect(400);
+});
+
+it('returns a 400 with an invalid password', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: "test@test",
+      password: "1"
+    })
+    .expect(400);
+});
+
+it('returns a 400 with missing email and password', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com'
+    })
+    .expect(400);
+
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      password: "password"
+    })
+    .expect(400);
+});
+
+it('disallows duplicate emails', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: "test@test.com",
+      password: "password"
+    })
+    .expect(201);
+
+    await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: "test@test.com",
+      password: "password"
+    })
+    .expect(400);    
+});
